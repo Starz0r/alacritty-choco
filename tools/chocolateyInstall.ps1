fe47ba28ca539e9339ee28b41990fb463043b055
Starz0r <starz0r@starz0r.com>
2019-02-25 16:41:12 -0600

    Bump to version 0.2.7

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $PackageName = 'alacritty'
-$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.2.6/Alacritty-v0.2.6.exe'
-$Checksum64 = '75131b9de16b9832fd62d7b61c63fc21da31c883f52f1e79a68b4893cab9dbfc6036a62e4873d004eb9a338eb664c074b4dedf04e7df2a3dff517fad5454eacd'
+$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.2.7/Alacritty-v0.2.7.exe'
+$Checksum64 = 'c8cb52a482660d2931b80aeefeba06579a79a1fc7a1a9c4bfce15fbdfe2bdeac4310680870184befe24fc84b7425064c37544dd1f55cb34c69b27defce6a3311'
 $ChecksumType64 = 'sha512'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallDir = Join-Path $(Get-ToolsLocation) $PackageName
-$UrlExtra64 = 'https://github.com/jwilm/alacritty/releases/download/v0.2.6/winpty-agent.exe'
+$UrlExtra64 = 'https://github.com/jwilm/alacritty/releases/download/v0.2.7/winpty-agent.exe'
 
 $desktop = [System.Environment]::GetFolderPath("Desktop")
 
