0cd0e53c7a3ce4629ed4d42e841633d7f94461fa
Starz0r <starz0r@starz0r.com>
2019-02-25 04:43:20 -0600

    Bump to version 0.2.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $PackageName = 'alacritty'
-$Url64 = 'https://github.com/jwilm/alacritty/releases/download/binaries/Alacritty.exe'
-$Checksum64 = 'D7498878DC5B964C8DAED02F4821FFB844F1204B395FB5E96F1E44638FE0317B1D541FFAC303C57A3C8F6C0591F561080459CB205F9C084626BCDA009DB07EC6'
+$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.2.3/Alacritty-v0.2.3.exe'
+$Checksum64 = '0f04ccff7902c603590b33c3b921cd3ca450df6932b745f048bb33eb9eb5db1e777f705c11afce5ef32171448d17bc10da6a53cd119f87eec5729b51e0c6d18a'
 $ChecksumType64 = 'sha512'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallDir = Join-Path $(Get-ToolsLocation) $PackageName
-$UrlExtra64 = 'https://github.com/jwilm/alacritty/releases/download/binaries/winpty-agent.exe'
+$UrlExtra64 = 'https://github.com/jwilm/alacritty/releases/download/v0.2.3/winpty-agent.exe'
 
 $desktop = [System.Environment]::GetFolderPath("Desktop")
 
@@ -13,7 +13,7 @@ $PackageArgs = @{
 	Url64 = $Url64
 	Checksum64 = $Checksum64
 	ChecksumType64 = $ChecksumType64
-	FileFullPath = Join-Path $InstallDir $([IO.Path]::GetFileName($Url64))
+	FileFullPath = Join-Path $InstallDir $([IO.Path]::GetFileName("alacritty.exe"))
 }
 Get-ChocolateyWebFile @PackageArgs
 
