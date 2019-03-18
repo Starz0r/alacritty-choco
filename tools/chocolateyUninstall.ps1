e64c8003ed396b3b982dc941e8c7ac486203154b
Starz0r <starz0r@starz0r.com>
2019-03-18 05:48:38 -0500

    Zip File Uninstall for version 0.2.8 and beyond

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -3,6 +3,7 @@ $InstallDir = Join-Path $(Get-ToolsLocation) $PackageName
 $desktop = [System.Environment]::GetFolderPath("Desktop")
 
 Uninstall-BinFile alacritty -path "$InstallDir\alacritty.exe"
+Uninstall-ChocolateyZipPackage -PackageName 'alacritty' -ZipFileName 'Alacritty-v0.2.9-windows.zip'
 
 Remove-Item $InstallDir
 Remove-Item "$desktop\Alacritty.lnk" -ErrorAction SilentlyContinue -Force | Out-Null
