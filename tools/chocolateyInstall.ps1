a5391bcaf3142b3cb38950ebb8237421e193e978
Starz0r <starz0r@starz0r.com>
2019-04-09 07:55:06 -0500

    Bump to version 0.3.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $PackageName = 'alacritty'
-$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.3.0-rc3/Alacritty-v0.3.0-rc3-windows-portable.zip'
-$Checksum64 = 'FEDB19FAB5CD0E277DD6C7C3319809AA5AA6FA97FA88D10BD4E12D4967F86071CF0DE3619BED08EC63E1DFA9DDD5A3163348B2FA389665373776DC041A092A33'
+$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.3.0/Alacritty-v0.3.0-windows-portable.zip'
+$Checksum64 = '4C9944BE2368FF119475F4B832728366EDFBF268439B4409DAB8CFDF08F359CD6A85B91971EFB2D891D4DF16FA21C5487909F85BBDF71D1B78C55AB5EFFE16C5'
 $ChecksumType64 = 'sha512'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallDir = Join-Path $(Get-ToolsLocation) $PackageName
