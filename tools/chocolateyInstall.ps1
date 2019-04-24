649bf40c30baa47714dff64ed49d0c00f98936e8
Starz0r <starz0r@starz0r.com>
2019-04-24 01:27:55 -0500

    Bump to version 0.3.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $PackageName = 'alacritty'
-$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.3.1/Alacritty-v0.3.1-windows-portable.zip'
-$Checksum64 = 'F486F93570E9163B11C1C3BA9B044A5C7AD3BA114E14F6ED5B1A0A818DD02FDD4449A71CD4E6FE77E63593F0D8D15FC91341F6DFC67C62DE0E002D8D2A21E06E'
+$Url64 = 'https://github.com/jwilm/alacritty/releases/download/v0.3.2/Alacritty-v0.3.2-windows-portable.zip'
+$Checksum64 = '66D97AC872842017C9A8F0FB25E6C626D8794E2AF3500A62BEFB9C1DEDCDABFF868A715DE6D5B305C704C5688CF21C0C3AE31316BA238B18FF53400EAF5897A9'
 $ChecksumType64 = 'sha512'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallDir = Join-Path $(Get-ToolsLocation) $PackageName
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -3,7 +3,7 @@ $InstallDir = Join-Path $(Get-ToolsLocation) $PackageName
 $desktop = [System.Environment]::GetFolderPath("Desktop")
 
 Uninstall-BinFile alacritty -path "$InstallDir\alacritty.exe"
-Uninstall-ChocolateyZipPackage -PackageName 'alacritty' -ZipFileName 'Alacritty-v0.3.1-windows-portable.zip'
+Uninstall-ChocolateyZipPackage -PackageName 'alacritty' -ZipFileName 'Alacritty-v0.3.2-windows-portable.zip'
 
 Remove-Item $InstallDir
 Remove-Item "$desktop\Alacritty.lnk" -ErrorAction SilentlyContinue -Force | Out-Null
